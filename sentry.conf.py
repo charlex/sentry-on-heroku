0b63fd8b7c365d2c6a275a7de27031d1b22278ba
charlex <charlex@athena.usa.tribune.com>
2017-04-13 14:21:32 -0700

    adding bucket region

--- sentry.conf.py
+++ sentry.conf.py
@@ -137,6 +137,7 @@ SENTRY_OPTIONS['filestore.options'] = {}
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_DEFAULT_ACL = 'private'
 
 ##############
