c48d03252e701904478bf11781ae0c9d8e2ffb8d
charlex <charlex@athena.usa.tribune.com>
2017-04-13 14:36:03 -0700

    filestore config

--- sentry.conf.py
+++ sentry.conf.py
@@ -131,14 +131,15 @@ SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
 # Any Django storage backend is compatible with Sentry. For more solutions see
 # the django-storages package: https://django-storages.readthedocs.org/en/latest/
 
-SENTRY_OPTIONS['filestore.backend'] = 'storages.backends.s3boto.S3BotoStorage'
-SENTRY_OPTIONS['filestore.options'] = {}
+SENTRY_OPTIONS['filestore.backend'] = 's3'
+SENTRY_OPTIONS['filestore.options'] = {
+    'access_key': os.environ.get('AWS_ACCESS_KEY_ID'),
+    'secret_key': os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    'bucket_name': os.environ.get('AWS_STORAGE_BUCKET_NAME')
+}
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-AWS_DEFAULT_ACL = 'private'
+# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+# AWS_DEFAULT_ACL = 'private'
 
 ##############
 # Web Server #
